ea792bedebb1bca58db3340d74548539d3d242f8
unknown <badger24a@gmail.com>
2023-03-09 09:07:56 +0100

    falta la pagina del carrito y ocultar la tabla en el index

--- script/script.js
+++ script/script.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const bodyTable = document.querySelector('.shopping tbody');
     const cardContainer = document.querySelector('#grid-card-container');
     const buyButton = document.querySelector('#buy-button');
-    const navButtons = document.querySelector('.button-nav-container');
+    const navButtons = document.querySelector('#button-nav-container');
+    const insertTotal = document.querySelector('#insert-total-td');
     //carrito.html
     const tableContainer = document.querySelector('.table-container tbody');
     const table = document.querySelector('.shopping');
@@ -32,37 +33,41 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(target);
             addProduct(target.id);//aqui se actualiza el local
             printIndexTable();//aqui se recoge el local y se pinta en la tabla
-
+            console.log('deberia haber pintado');
         }
-        if (target.matches('.deleteProduct')){
+        if (target.matches('.deleteProduct')) {
             console.log(target);
             deleteProduct(target.id);
             printIndexTable();
         }
-        if(target.matches('#buy-button')){
+        if (target.matches('#buy-button')) {
             console.log(target);
+            location.href='../carrito.html';
             //location.href='./carrito.html';
         }
-    }
-
-
-    )
+        if (target.matches('.page-button')) {//id botones=num pagina, class botones=page-button
+            console.log(target);
+            printCards(target.id);
+        }
+    })
 
 
     //**************************LLAMADA A LA API NOTA: enlace: https://dummyjson.com/products
-    const url = 'https://dummyjson.com/products';
-    const consulta = async (id) => {
+    const limit = 9;
+    const consulta = async (id, skip) => {
+        let url;
         try {
-            let peticion;
-            if (!id) {//un solo elemento
-                peticion = await fetch(url)//peticion sera a todos los productos si el id del producto que pedimos no existe
-            } else {
-                peticion = await fetch(`${url}/${id}`);//si id si que existe nos mandara la info del producto concreto
+            if (id) {
+                url = `https://dummyjson.com/products/${id}`;
+            } else if (!skip) {//si no hay limite ni skip
+                url = `https://dummyjson.com/products?limit=${limit}&skip=0`;
+            } else if (skip) {
+                url = `https://dummyjson.com/products?limit=${limit}&skip=${skip}`;
             }
+            console.log(url);
+            let peticion = await fetch(url);
             if (peticion.ok) {//si el ok es true se hace la conversion con los datos pedidos
-                const respuesta = await peticion.json();
-
-                console.log(respuesta);
+                let respuesta = await peticion.json();
                 return {
                     ok: true,
                     respuesta
@@ -102,18 +107,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //----------------------------INDEX
     //pintar cards NOTA: icono estrellas <i class="fa-thin fa-star"></i>
-    const printCards = async () => {
-        let allProducts = await consulta();
-        console.log(allProducts);//{ok:true respuesta:objeto}
-        const arrayProducts = allProducts.respuesta.products;//los productos vienen en un array, son un total de 30
-        console.log(arrayProducts);
-        //tres primeros
-        arrayProducts.forEach(({ id, title, description, price, rating, images }) => {
+    const printCards = async (actualPage) => {//consulta(prudporpag,salto de elementos,id)
+        //let numTotalProducts = arrayProducts.length;
+        cardContainer.innerHTML = '';
+        let waitProducts;
+        if (actualPage == undefined) {
+            actualPage = 1;
+            waitProducts = await consulta(undefined,);//9,(actualPage-1)*9)
+        } else {
+            waitProducts = await consulta(undefined, (actualPage - 1) * 9)
+            console.log(waitProducts);
+        }
+        const visibleProducts = waitProducts.respuesta.products;
+        const totalProducts = waitProducts.respuesta.total;
+        printPageButtons(totalProducts);
+        visibleProducts.forEach(({ id, title, description, price, rating, images }) => {
             const card = document.createElement('DIV');
             card.className = 'grid-item';
             const divImg = document.createElement('DIV');
             const productImg = document.createElement('IMG');
-            productImg.className='card-img';
+            productImg.className = 'card-img';
             productImg.src = images[0];
             divImg.append(productImg);
             card.append(divImg);
@@ -122,23 +135,42 @@ document.addEventListener('DOMContentLoaded', () => {
             const titleProduct = document.createElement('H4');
             titleProduct.innerHTML = `${title} &nbsp <span>${price}$</span>`;
             const descriptionProduct = document.createElement('P');
+            descriptionProduct.className = 'texto-ajustado';
             descriptionProduct.textContent = description;
             divInfo.append(titleProduct, descriptionProduct);
             card.append(divInfo);
 
             card.append(printStars(rating));
 
+            const divCardButton = document.createElement('DIV');
+            divCardButton.classList = 'button-container-class';
             const buttonProd = document.createElement('BUTTON');
+            divCardButton.append(buttonProd);
             buttonProd.innerHTML = `Add to cart`;
             buttonProd.className = 'addProduct';
             buttonProd.id = id;
-            card.append(buttonProd);
+            card.append(divCardButton);
 
             fragment.append(card);
         })
         cardContainer.append(fragment);
     }
 
+    const printPageButtons = (total) => {
+        navButtons.innerHTML = '';
+        let pages = parseInt(total / 9);
+        let rest = total % 9;
+        if (rest != 0) {
+            pages = pages + 1;
+        }
+        for (let i = 1; i <= pages; i++) {
+            const navButton = document.createElement('BUTTON');
+            navButton.className = 'page-button';
+            navButton.textContent = `${i}`;
+            navButton.id = i
+            navButtons.append(navButton);
+        }
+    }
 
     //pintar estrellas funcion
     const printStars = (rating) => {//siendo productos el array con todos los productos y su info
@@ -150,7 +182,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const container = document.createElement('DIV');
             const star = document.createElement('img');
             star.src = fullStar;
-            star.className='img-star';
+            star.className = 'img-star';
             container.append(star);
             divStars.append(container);
         } for (let i = 0; i < 5 - Math.round(rating); i++) {
@@ -163,10 +195,12 @@ document.addEventListener('DOMContentLoaded', () => {
         return divStars;
     }
     //pintar la tabla del index los objetos que anadan a la cesta o en su defecto actualizarlos
-    const printIndexTable = () => {//si no pasamos un id a la funcion pinta lo del localStorage nada mas inicial la pagina
+    const printIndexTable = async () => {//si no pasamos un id a la funcion pinta lo del localStorage nada mas inicial la pagina
         bodyTable.innerHTML = '';
-        let arrayLocal = getLocal();
+        let arrayLocal = await getLocal();
         console.log(arrayLocal);
+        let arraysubtotales = [];
+        let total = 0;
         if (arrayLocal.length != 0) {
             arrayLocal.forEach(({ id, images, title, price, contador }) => {
                 const row = document.createElement('TR');
@@ -174,12 +208,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const tdImage = document.createElement('TD');
                 const divthumb = document.createElement('div');
-                divthumb.className='thumb';
+                divthumb.className = 'thumb';
                 const imageThumb = document.createElement('IMG');
                 imageThumb.src = images[4];
-                imageThumb.className='thumb';
+                imageThumb.className = 'thumb';
                 divthumb.append(imageThumb);
                 tdImage.append(divthumb);
+
                 const emptyTD = document.createElement('TD');
 
                 const tdName = document.createElement('TD');
@@ -206,12 +241,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 tdMore.append(moreButton);
 
                 const tdSubtotal = document.createElement('TD');
-                tdSubtotal.innerHTML = `${price}$ * ${contador} = ${price * contador}`;
+                let subtotal = price * contador;
+                tdSubtotal.innerHTML = `${price * contador}`;
                 row.append(emptyTD, tdImage, tdName, tdPrice, tdLess, tdQuantity, tdMore, tdSubtotal)
                 fragment.append(row);
-            })
-            bodyTable.append(fragment);
 
+                arraysubtotales.push(subtotal);
+            })
+            for (let i = 0; i < arraysubtotales.length; i++) {
+                total += arraysubtotales[i];
+            }
+            bodyTable.append(fragment);
+            console.log(total)
+
+            const tdTotal = document.createElement('TD');
+            insertTotal.innerHTML = `
+            <td><button id="buy-button">BUY</button></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>TOTAL</td>
+            <td>${total}</td>`
         }
         //en caso de que si pasemos id, sera porque se ha pulsado un boton de add o delete y tenemos repintar la tabla con el local actualizado, bajamos el local y lo pintamos
 
@@ -219,49 +271,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //pintar objetos en la tabla index
     const addProduct = async (idProd) => {
-        let consult = await consulta();
-        let allProducts = consult.respuesta.products;
-        //buscamos en la API el id del objeto que hemos anadido al carro
-        console.log(allProducts);
-        let obj = allProducts.find((prod) => prod.id == idProd);
-        console.log('id del obj seleccionado' + obj.id);//si sale el objeto
+        let consult = await consulta(idProd, null);//busca el producto concreto por id
+        let theProduct = consult.respuesta;//nos da un objeto concreto
         //buscamos si en el array de productos seleccionados ya existe el objeto que hemos metido al carrito
-        const foundList = arraySelectedProducts.find((prods) => prods.id == obj.id);//deberia de encontrar un objeto si es que lo encuentra
+        arraySelectedProducts = getLocal();
+        const foundList = arraySelectedProducts.find(({ id }) => id == theProduct.id);//deberia de encontrar un objeto si es que lo encuentra
+        console.log(arraySelectedProducts);
         console.log(foundList);
         if (foundList == undefined) {
-            obj.contador = 0;
-            obj.contador++;//si no encuentra el elemento en la lista de seleccionados, anade al objeto una nueva propiedad llamada contador y le da el valor de 1
-            arraySelectedProducts.push(obj);
-            setLocal();
-            console.log('anadido obj al array' + arraySelectedProducts);//va bien
+            theProduct.contador = 0;
+            theProduct.contador++;//si no encuentra el elemento en la lista de seleccionados, anade al objeto una nueva propiedad llamada contador y le da el valor de 1
+            arraySelectedProducts.push(theProduct);
+            setLocal();//sube el arraySelectedProducts al local
+            console.log('anadido obj al array');//va bien
         } else if (foundList != undefined) {//en caso de que si exista en la cesta un producto de minmo indice tendremos que añadirle mas uno al contador 
             foundList.contador++;
             console.log('+1 contador');
             setLocal();
         }
+        arraySelectedProducts = getLocal();
         console.log(arraySelectedProducts);
     }
 
     const deleteProduct = async (idProd) => {
+        arraySelectedProducts = getLocal();
         const foundList = arraySelectedProducts.find((prods) => prods.id == idProd);
         foundList.contador--;
         console.log(foundList);
+        let index = arraySelectedProducts.findIndex((prod) => {
+            if (prod.id == foundList.id) {
+                return true;
+            }
+        })
         if (foundList.contador > 0) {//si el id del producto está en nuestra cesta, le restamos uno a su contador
-            let index = arraySelectedProducts.findIndex((prod) => {            //buscamos el indice dentro del array de productos que tenga el mismo id que el que acaban de quitar de la cesta
-                if (prod.id == foundList.id) {
-                    return true;
-                }
-            });
             if (index != -1) {
                 arraySelectedProducts[index] = foundList;
                 setLocal();
             }
             console.log(arraySelectedProducts);
-        } else if (foundList.contador = 0) {
-            arraySelectedProducts.splice(index,1)
+        } else if (foundList.contador < 1 || null) {
+
+            arraySelectedProducts.splice(index, 1)
+            console.log(arraySelectedProducts);
             setLocal();
         }
-
+        arraySelectedProducts = getLocal();
     }
 
     //funcion visibilidad de la tabla
@@ -269,8 +323,6 @@ document.addEventListener('DOMContentLoaded', () => {
         tableIndex.classList.toggle('visible');
         console.log('visible');
     }
-
-    //pintar botones para navegar entre los productos
 
     //----------------------------CARRITO.HTML
     //pintar tabla segun lo que haya almacenado en el localhost
@@ -289,14 +341,15 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const init = () => {
-        if(location.pathname='/index.html'){
-        printIndexTable();
-        printHeaderButton();
-        printCards();
+        if (location.pathname = '/index.html') {
+            printIndexTable();
+            printHeaderButton();
+            printCards();
+        }
+        else if (location.pathname = '/carrito.html') {
+            printHeaderButton();
+            printIndexTable();
+        }
     }
-    else if(location.pathname='/carrito.html'){
-        printHeaderButton();
-        printIndexTable();
-    }}
     init();
 })//LOAD
